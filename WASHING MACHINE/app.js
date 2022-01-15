6df3a6f3deb5de8803c5fe1d19c3f5fb6da34105
SJRXD <rohr.olasz@gmail.com>
2022-01-14 22:58:22 -0800

    washing machine code

---
+++ WASHING MACHINE/app.js
@@ -0,0 +1,11 @@
+// Load Machine
+// Add Detergent or Bleach
+// Add Fabric Softener
+// Select Temp
+// Select Spin Speed
+// Select Rinses
+
+// Want a loop
+// Loop through idk Clothes
+// Iterations until clean???
+
