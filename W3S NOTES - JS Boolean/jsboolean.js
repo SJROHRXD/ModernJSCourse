0e7502e11a698ea4230cafd6e545f7f8e4dd7f76
SJRXD <rohr.olasz@gmail.com>
2022-01-14 21:42:12 -0800

    boolean references

---
+++ W3S NOTES - JS Boolean/jsboolean.js
@@ -0,0 +1,57 @@
+// JavaScript Boolean Reference ✨
+
+// JavaScript Booleans 🌷🌿🌻
+
+// JS Booleans can have one of two values: true or false
+
+// Boolean Function
+
+// Use Boolean() to find out if an expression is true
+
+let thisBoo = Boolean(4 > 2);
+console.log(thisBoo);
+
+// output
+// true
+
+// Boolean Methods
+// Method - - - Description
+// toString() - - - Converts a boolean value to a string, returns the result
+// valueOf() - - - Returns the primative value of a boolean
+
+// Boolean Properties
+// Property - - - Description
+// constructor - - - Returns the function that created JS Boolean prototype
+// prototype - - - Allows you to add properties and methods to the Boolean prototype
+
+// Constructor
+
+// Returns the function that created the Boolean prototype
+
+// function Boolean() { [native code] }
+
+// 🌿 🌼 🌷 - E X A M P L E - 🌿 🌼 🌷
+
+let booCh = false;
+let bussy = booCh.constructor;
+
+console.log(bussy);
+
+// output
+// ƒ Boolean() { [native code] }
+
+// 🌿 🌼 🌷 🌿 🌼 🌷 🌿 🌼 🌷 🌿 🌼 🌷 
+
+// Prototype
+
+// Allows you to add new properties and methods to booleans
+// A property available with all JS objects
+
+// You should not change the prototype of built in JavaScript datatypes ⚠
+
+// 🌿 🌼 🌷 - E X A M P L E - 🌿 🌼 🌷
+// 🌿 🌼 🌷 🌿 🌼 🌷 🌿 🌼 🌷 🌿 🌼 🌷
+
+// toString()
+
+// valueOf()
