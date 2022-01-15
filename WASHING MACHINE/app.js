ca8b734bbce9d95ebd0c54021445872bf39bc913
SJRXD <rohr.olasz@gmail.com>
2022-01-14 23:03:12 -0800

    washing machine code x2

--- WASHING MACHINE/app.js
+++ WASHING MACHINE/app.js
@@ -1,11 +1,23 @@
 // Load Machine
 // Add Detergent or Bleach
 // Add Fabric Softener
+
+// is machine loaded? t/f? then run machine okay
+
 // Select Temp
+// temp determines?
 // Select Spin Speed
-// Select Rinses
+// spin speed determines???
+// Soil level
+// soil level determines how many iterations
+// Select Rinses (1 extra rinse, 2 extra rinses)
+// how many iterations per extra rinse
+
+// is temp, spin speed, rinse selected? then...
+// defaults? alert
 
 // Want a loop
 // Loop through idk Clothes
 // Iterations until clean???
 
+// 
