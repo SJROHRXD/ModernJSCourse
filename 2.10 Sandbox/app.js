262eab650170083887515f7b12cb138eaa238d7f
SJRXD <rohr.olasz@gmail.com>
2022-01-20 15:57:40 -0800

    Modern JS 2.10, Math

---
+++ 2.10 Sandbox/app.js
@@ -0,0 +1,31 @@
+// JavaScript - Numbers and the Math Object 🌼🌿🌷
+
+const num1 = 100;
+const num2 = 50;
+let = val;
+
+// Simple math
+
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+// Already gone over the Math Object so this is more of a quick ref
+
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8,2);
+val = Math.min(2,33,4,5);
+val = Math.max(2,33,4,5);
+val = Math.random();
+
+val = Math.random() * 20; // give us random number w a decimal between 0 and 19 (0, inclusive, 20, exclusive)
+val = Math.floor(Math.random() * 20 + 1); // 20 inclusive
